f317bf70c40a62ff2de59209f9480219e9506666
Alexander Standke <alex.standke@shopify.com>
2022-04-28 13:11:11 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 if [ ! -z "${SPIN}" ]; then
-  sudo apt install -y ripgrep fd-find neovim
-  cat ./addtorc >> "${HOME}/.zshrc"
-
-  [ ! -f "${HOME}/.config/nvim" ] && sh -c "$(git clone https://github.com/luan/nvim ~/.config/nvim)"
-
-  nvim +PlugInstall +qall
-  nvim +UpdateRemotePlugins +qall
-  nvim +GoInstallBinaries +qall
+  sudo apt install -y ripgrep fd-find
 fi
 
