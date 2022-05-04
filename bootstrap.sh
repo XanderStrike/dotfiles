da3d90fd76fcb0df80fe4d88ba84f58e19e6275d
Alexander Standke <alex.standke@shopify.com>
2022-05-04 09:19:14 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 if [ ! -z "${SPIN}" ]; then
-  sudo apt install -y ripgrep fd-find
+  if ! command -v rg &> /dev/null; then
+    sudo apt install -y fd-find
+  fi
+  
+  if ! command -v rg &> /dev/null; then
+    sudo apt-get install -y ripgrep
+  fi
+
+  if ! command -v fzf &> /dev/null; then
+    sudo apt-get install -y fzf
+  fi
   
   cat ./addtorc >> "${HOME}/.zshrc"
 fi
